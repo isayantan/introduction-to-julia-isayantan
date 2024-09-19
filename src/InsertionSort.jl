a362e1a80e6bd0f12dbb36652d21d97229e3a76a
isayantan <sayantan.choudhury4@gmail.com>
2024-09-19 18:55:15 -0400

    Update insertion_sort

--- src/InsertionSort.jl
+++ src/InsertionSort.jl
@@ -5,6 +5,16 @@ using Random
 
 function insertion_sort!(array::Array{Int, 1})
     # Insert your code here
+    for i in 2:length(arr)
+        key = arr[i]
+        j = i - 1
+        while j > 0 && arr[j] > key
+            arr[j + 1] = arr[j]
+            j -= 1
+        end
+        arr[j + 1] = key
+    end
+    return arr   
 end
 
 function check_sort(N::Int = 100)
